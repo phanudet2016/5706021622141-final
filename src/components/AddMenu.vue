f4179506ea9c00390aa38dd323f28274992c4092
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2017-05-26 12:10:59 +0700

    first commit

---
+++ src/components/AddMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <div class="col-lg-6">
+     <div class="input-group">
+       <input v-model="name" type="text" class="form-control" placeholder="ชื่อวิชา">
+       <input v-model="phone" type="text" class="form-control" placeholder="หน่วยกิต">
+       <input v-model="email" type="text" class="form-control" placeholder="เกรด">
+       <span class="input-group-btn">
+
+         <button @click="add(name, phone, email)" class="btn btn-default" type="button">+Add</button>
+       </span>
+     </div><!-- /input-group -->
+   </div><!-- /.col-lg-6 --><br><br><br>
+   <center>
+  </div><button @click="" class="btn btn-default" type="button" style="background-color: #4CAF50;font-size: 16px;padding: 15px 32px;color: white;">คำนวณ</button></center>
+</template>
+
+<script>
+export default {
+  props: ['add'],
+  data () {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Menus.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="hello">
+  <table class="table">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>ชื่อวิชา</th>
+      <th>หน่วยกิต</th>
+      <th>เกรด</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  </table>
+  <!-- <v-for="list in menu"> -->
+    <list v-for="list in menu" :list="list" :remove="remove" :index="$index"></list>
+  </div>
+</div>
+</template>
+
+<script>
+import List from './List'
+export default {
+  props: ['menu', 'remove'],
+  data () {
+    return {}
+  },
+  components: {
+    List
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
